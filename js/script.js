a928996644ce35eeec848ccd8bfd48e584e42022
Adrian Sadowski <sadow.adrian@gmail.com>
2021-07-08 19:32:25 +0200

    add active to clicked link

--- js/script.js
+++ js/script.js
@@ -3,21 +3,24 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
-  const titleClickHandler = function(){
+  const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' for all arcitle links */
+    /* [DONE] remove class 'active' for all arcitle links */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+    for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
     }
 
     /* add class 'active' to the clicked link*/
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all arcitles */
-    const activeArticles = document.querySelectorAll('.active');
+    /* [DONE] remove class 'active' from all arcitles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
