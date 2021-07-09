5443b00da3c76d4b02df5d9cd4117c750e61664d
Adrian Sadowski <sadow.adrian@gmail.com>
2021-07-09 20:59:59 +0200

    remove contents of tittleList

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-'use strict';
+{
+  'use strict';
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
+
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -44,3 +46,32 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  const optArticleSelector  = '.post'
+    optTitleSelector = '.post-title'
+    optTitleListSelector = '.titles'
+
+  function generateTitleLinks(){
+    
+  /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+  
+  }
+
+  generateTitleLinks();
+  function clearMessages(){
+    document.getElementById('messages').innerHTML = '';
+  }
+}
