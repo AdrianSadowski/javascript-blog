c917dc33725c23c14aed2050128cdb66dc74d6cb
Adrian Sadowski <sadow.adrian@gmail.com>
2021-07-10 12:22:56 +0200

    add JS links

--- js/script.js
+++ js/script.js
@@ -39,38 +39,53 @@
 
     /* add class 'active' to the correst arcitle */
     targetArticle.classList.add('active');
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+  } 
 
   const optArticleSelector  = '.post'
     optTitleSelector = '.post-title'
     optTitleListSelector = '.titles'
 
+
+  
   function generateTitleLinks(){
     
   /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
   /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    //console.log('optArticleSelector', optArticleSelector);
+    for(let article of articles){
 
     /* get the article id */
-
+      const articleId = article.getAttribute('id');
+      //console.log('articleId', articleId);
+      
     /* find the title element */
-
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  
     /* get the title from the title element */
-
+    
     /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      //console.log('linkHTML', linkHTML);
 
     /* insert link into titleList */
-  
+      html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+    //console.log('html', html);
   }
 
   generateTitleLinks();
+  const links = document.querySelectorAll('.titles a');
+  console.log('links', links)
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+
   function clearMessages(){
     document.getElementById('messages').innerHTML = '';
   }
